8e513bf00dd4be175e02b8df8fd5dd30d959de6f
nebrog <piska@mail.ru>
2022-03-17 19:16:13 +0300

    Добавил состояния загрузки и ошибки

--- app/src/main/java/com/example/news/MainActivity.java
+++ app/src/main/java/com/example/news/MainActivity.java
@@ -1,14 +1,16 @@
 package com.example.news;
 
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
-
-import android.os.Bundle;
-import android.util.Log;
-import android.view.Window;
-import android.widget.LinearLayout;
 
 import com.example.news.data.NewsAdapter;
 import com.example.news.data.api.NewsAPI;
@@ -16,33 +18,59 @@ import com.example.news.data.models.ArticlesPOJO;
 
 import io.reactivex.SingleObserver;
 import io.reactivex.android.schedulers.AndroidSchedulers;
-
-import io.reactivex.Scheduler;
-import io.reactivex.Single;
 import io.reactivex.disposables.Disposable;
 import io.reactivex.schedulers.Schedulers;
-import retrofit2.Call;
-import retrofit2.Callback;
-import retrofit2.Response;
 
 public class MainActivity extends AppCompatActivity {
-    NewsAPI newsAPI = NewsAPI.getApi();
-    NewsAdapter newsAdapter = new NewsAdapter();
+
+    private final NewsAPI newsAPI = NewsAPI.getApi();
+    private final NewsAdapter newsAdapter = new NewsAdapter();
+
+    private RecyclerView recyclerView;
+    private ProgressBar progressBar;
+    private Button refreshButton;
+    private TextView errorText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        RecyclerView recyclerView = findViewById(R.id.recycler);
-        LinearLayoutManager linearLayout =new LinearLayoutManager(this);
+        findAllViews();
+        setupRecycler();
+        setupListeners();
+        loadNews();
+    }
+
+    private void findAllViews() {
+        recyclerView = findViewById(R.id.recycler);
+        errorText = findViewById(R.id.error_text);
+        refreshButton = findViewById(R.id.refresh_button);
+        progressBar = findViewById(R.id.progress_bar);
+    }
+
+    private void setupRecycler() {
+        LinearLayoutManager linearLayout = new LinearLayoutManager(this);
         recyclerView.setLayoutManager(linearLayout);
         recyclerView.setAdapter(newsAdapter);
-        loadNews();
-
-
     }
-    public void loadNews(){
-        newsAPI.loadNews("ru", "Russia", "347d40fa99b74141a0e40d3ce326b6cd")
+
+    private void setupListeners() {
+        refreshButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                loadNews();
+            }
+        });
+    }
+
+    public void loadNews() {
+        errorText.setVisibility(View.GONE);
+        recyclerView.setVisibility(View.GONE);
+        refreshButton.setVisibility(View.GONE);
+        progressBar.setVisibility(View.VISIBLE);
+
+        newsAPI
+                .loadNews("ru", "Russia", "347d40fa99b74141a0e40d3ce326b6cd")
                 .observeOn(AndroidSchedulers.mainThread())
                 .subscribeOn(Schedulers.io())
                 .subscribe(new SingleObserver<ArticlesPOJO>() {
@@ -54,15 +82,17 @@ public class MainActivity extends AppCompatActivity {
                     @Override
                     public void onSuccess(@NonNull ArticlesPOJO articlesPOJO) {
                         newsAdapter.setNews(articlesPOJO.articles);
-
+                        progressBar.setVisibility(View.GONE);
+                        recyclerView.setVisibility(View.VISIBLE);
                     }
 
                     @Override
                     public void onError(@NonNull Throwable e) {
-                        Log.e("nebrog",e.toString(),e);
-
+                        Log.e("nebrog", e.toString(), e);
+                        progressBar.setVisibility(View.GONE);
+                        errorText.setVisibility(View.VISIBLE);
+                        refreshButton.setVisibility(View.VISIBLE);
                     }
                 });
     }
-
 }
